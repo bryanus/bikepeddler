b9b737ac042ea9a15a153d5be933da73a3c48a91
Bryan Heu <bheuclickbooq@gmail.com>
2013-06-19 12:20:56 -0700

    edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,21 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
+	def show
+		@user = User.find(params[:id]) #can also use current_user.id. which is better?
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+			redirect_to user_path(@user)
+		else
+      render 'edit'
+    end
+	end
 
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ActiveRecord::Base
   attr_accessible :description, :price, :title, :images
 
   validates :title,  :presence => true
-  validates :price,  :presence => true
+  validates :price,  :presence => true 
+  #need to add formatting validations for price
   validates :description,  :presence => true
  
   has_many :images
