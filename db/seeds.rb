b21f35cb3ddea934a84e15cc0f6aadc69c8655bc
Bryan Heu <bheuclickbooq@gmail.com>
2013-07-08 16:57:39 -0700

    created seed for Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = ["Bikes","Parts","Forks","Wheelsets","Vintage","Other"]
+
+categories.each do |category|
+	Category.create(name: category)
+end
