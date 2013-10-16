2240fe2f2cfae3ffe3d236d50d918449d2f67f7d
Bryan Heu <bheuclickbooq@gmail.com>
2013-10-16 16:29:53 -0700

    put mailer vars into yaml ignore file and fix form css some more

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "kukumlima@gmail.com",
-  :password             => "Bry@nus!",
+  :user_name            => ENV["MAILER_USERNAME"],
+  :password             => ENV["MAILER_PASSWORD"],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
