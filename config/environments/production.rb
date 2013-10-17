bb2672185c2723239bdf34a86a4502031e9f8d90
Bryan Heu <bheuclickbooq@gmail.com>
2013-10-17 10:11:08 -0700

    config prod env to add mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Bikepeddler::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Other config items omitted.
+  config.action_mailer.default_url_options = { :host => "heavypeddler.herokuapp.com" }
 end
