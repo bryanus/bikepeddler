38f64a223b1b693953cca29e2395e8aa3f5978d0
Bryan Heu <bheuclickbooq@gmail.com>
2013-11-05 13:52:10 -0800

    change path to rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ server "192.241.223.181", :web, :app, :db, primary: true
 set :application, "heavypeddler"
 set :user, "deploy"
 set :port, 22
-set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_to, "/home/rails/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, true
 
