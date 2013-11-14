6c3c337f3b2829d6e9ff4e77e06aaecc3640d1f9
Bryan Heu <bheuclickbooq@gmail.com>
2013-11-14 14:47:15 -0800

    move assets in public dir to asset pipeline instead

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,11 +35,11 @@ namespace :deploy do
     desc "precompile assets"
     task :precompile, :roles => :web, :except => { :no_release => true } do
           from = source.next_revision(current_revision)
-          # if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+          if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
             run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-          # else
-          #   logger.info "Skipping asset pre-compilation because there were no asset changes"
-          # end
+          else
+            logger.info "Skipping asset pre-compilation because there were no asset changes"
+          end
       end
   end
 
