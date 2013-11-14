3a64f0bbc72f733b912d1d3b4b9ae4581ec87dba
Bryan Heu <bheuclickbooq@gmail.com>
2013-11-13 16:40:03 -0800

    add asset procompile deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,16 +21,8 @@ ssh_options[:forward_agent] = true
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
-  namespace :assets do
-        task :precompile, :roles => :web, :except => { :no_release => true } do
-          from = source.next_revision(current_revision)
-          if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-            run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-          else
-            logger.info "Skipping asset pre-compilation because there were no asset changes"
-          end
-      end
-    end  
+
+        
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
@@ -40,6 +32,15 @@ namespace :deploy do
 	  task :seed do
 	    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
 	  end
+    desc "precompile assets"
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+          from = source.next_revision(current_revision)
+          if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+            run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+          else
+            logger.info "Skipping asset pre-compilation because there were no asset changes"
+          end
+      end
   end
 
   task :setup_config, roles: :app do
