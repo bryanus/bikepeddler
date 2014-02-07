d1a3b15d68d197a86ed4d4d4904b030ee859f280
Bryan Heu <teamriab@gmail.com>
2014-02-07 14:09:47 -0800

    update web server IP to use Elastic IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server "ec2-54-219-23-161.us-west-1.compute.amazonaws.com", :web, :app, :db, primary: true
+server "54.241.24.21", :web, :app, :db, primary: true
 
 set :rails_env,  'production'
 # set :application, "heavypeddler"
